b012ffe884fa361759e96063ae86ea698ec881ea
Santoshdandin <santusb427@gmail.com>
2022-10-13 19:51:49 +0530

    added portfolio

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>SANTOSH</h1>
+       <img class="image" src="https://avatars.githubusercontent.com/u/97119715?v=4"/>
       </header>
     </div>
   );
